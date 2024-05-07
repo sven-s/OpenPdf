ccd9b23786868c476163c82ac4686e2e04b35017
Sven Sönnichsen <sven@sven-s.de>
2024-05-07 14:58:38 +0200

    Add maui project

---
+++ OpenPDF/Platforms/Android/MainActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+
+namespace OpenPDF;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+    LaunchMode = LaunchMode.SingleTop, Exported = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[IntentFilter([Intent.ActionView], Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable], DataMimeType = @"application/pdf")]
+public class MainActivity : MauiAppCompatActivity
+{
+}
